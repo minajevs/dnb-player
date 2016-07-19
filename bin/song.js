65e39bc77f28fe4671c622fa6987ab602c4c7031
dexiesharp <d3x133@gmail.com>
2016-07-19 09:15:28 +0300

    ES6 fix for heroku

--- bin/song.js
+++ bin/song.js
@@ -1,12 +1,19 @@
 var utils = require('./utils');
 
-class Song{
-    constructor(name, url, tags = []){
-        this.name = name;
-        this.url = url;
-        this.tags = tags;
-        this.id = utils.getGuid();
-    }
+//class Song{
+//    constructor(name, url, tags = []){
+//        this.name = name;
+//        this.url = url;
+//        this.tags = tags;
+//        this.id = utils.getGuid();
+//    }
+//}
+
+function Song(name, url, tags){
+    this.name = name;
+    this.url = url;
+    this.tags = tags;
+    this.id = utils.getGuid();
 }
 
 module.exports = Song;
