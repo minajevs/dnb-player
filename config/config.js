75c535282e1e682b7ce22dda1699d1d7d17b1ea0
dexiesharp <d3x133@gmail.com>
2016-07-21 12:47:07 +0300

    fix for localhost

--- config/config.js
+++ config/config.js
@@ -6,6 +6,7 @@ config.web = {};
 config.soundcloud = {};
 
 config.web.port = process.env.PORT || '3000';
+config.web.hostname = config.debug ? '//localhost:' + config.web.port : '//dnb.dexie.me';
 
 config.soundcloud.clientID = 'c2a1e9215e0e6f4dc03afe023ee9e212';
 
